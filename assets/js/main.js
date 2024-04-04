5ba91f6b0e61318a71a7e783c3af4a048d531acc
Parsa Vesali <parsavesali84@gmail.com>
2024-04-04 13:52:17 +0330

    project updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
 'use strict'
+const $ = document
+ const header =  $.querySelector('header')
+ const nav =  $.querySelector('nav')
+ const navbarMenuBtn =  $.querySelector('.navbar-menu-btn')
+
+ const navbarFrom = $.querySelector('.navbar-form')
+ const navbarFromClose = $.querySelector('.navbar-form-close')
+ const navbarFromBtn = $.querySelector('.navbar-search-btn')
+
+
+ function navisActive () {
+    header.classList.toggle('active')
+    nav.classList.toggle('active')
+    navbarMenuBtn.classList.toggle('active')
+ }
+
+ navbarMenuBtn.addEventListener('click' , navisActive)
+
+
+
+ const searchBarIsActive = ()=> navbarFrom.classList.toggle('active')
+
+ navbarFromBtn.addEventListener('click' , searchBarIsActive)
+ navbarFromClose.addEventListener('click' , searchBarIsActive)
